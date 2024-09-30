2b614d52c2f7071d9df5b25da7ddaa88ff9f0074
Your Name <youremail@domain.com>
2024-09-30 15:46:00 +0300

    map method

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => 
+    tutorial
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ')
+);
+
+  }
